7f22615f85e837d1730c84932436368a2ceea9ea
Marco Schlicht <git@privacymail.dev>
2021-08-21 23:22:49 +0200

    initial commit

---
+++ django_setup/test_settings.py
@@ -0,0 +1,72 @@
+import sys
+from datetime import datetime
+from distutils import util
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'lab_orchestrator_lib_django_adapter.apps.LabOrchestratorLibDjangoAdapterConfig',
+)
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'django_setup.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter'
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+DEBUG = True
+SECRET_KEY = "secret_key_for_testing"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
---
+++ django_setup/urls.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'docker_image': reverse('lab_orchestrator:docker_image-list', request=request, format=format),
+        'lab': reverse('lab_orchestrator:lab-list', request=request, format=format),
+        'lab_instances': reverse('lab_orchestrator:lab_instance-list', request=request, format=format),
+    })
+
+
+@api_view(['GET'])
+def root(request, format=None):
+    return Response({
+        'api': reverse('api_root', request=request, format=format),
+    })
+
+
+urlpatterns = [
+    path('', root, name='root'),
+    path('api/', api_root, name='api_root'),
+    path('api/', include('lab_orchestrator_lib_django_adapter.urls')),
+]
+
+urlpatterns += [
+    path('admin/', admin.site.urls),                    # contains the admin web-ui
+    path('api-auth/', include('rest_framework.urls')),  # contains login and logout for the api web-ui
+]
