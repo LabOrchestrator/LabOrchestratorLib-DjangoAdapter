5f7c70444fb6369a8189a318700a2f0d3f2cdc50
Marco Schlicht <git@privacymail.dev>
2021-08-23 13:33:40 +0200

    using default id fields in model and added to string

--- lab_orchestrator_lib_django_adapter/apps.py
+++ lab_orchestrator_lib_django_adapter/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class LabOrchestratorLibDjangoAdapterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'lab_orchestrator_lib_django_adapter'
--- lab_orchestrator_lib_django_adapter/models.py
+++ lab_orchestrator_lib_django_adapter/models.py
@@ -3,22 +3,28 @@ from django.contrib.auth import get_user_model
 
 
 class DockerImageModel(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=32, unique=True, null=False)
     description = models.CharField(max_length=128, null=True)
     url = models.CharField(max_length=256, null=False)
 
+    def __str__(self):
+        return f"{self.name}: {self.url} ({self.pk})"
+
 
 class LabModel(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=32, unique=True, null=False)
     namespace_prefix = models.CharField(max_length=32, unique=True, null=False)
     description = models.CharField(max_length=128, null=True)
-    docker_image_id = models.ForeignKey(DockerImageModel, on_delete=models.DO_NOTHING, null=True, related_name="labs")
+    docker_image = models.ForeignKey(DockerImageModel, on_delete=models.DO_NOTHING, null=False, related_name="labs")
     docker_image_name = models.CharField(max_length=32, null=False)
+
+    def __str__(self):
+        return f"{self.name}: {self.docker_image} ({self.docker_image_name}) ({self.pk})"
 
 
 class LabInstanceModel(models.Model):
-    id = models.IntegerField(primary_key=True)
-    lab_id = models.ForeignKey(LabModel, on_delete=models.CASCADE, null=False, related_name="lab_instances")
-    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=False, related_name="lab_instances")
+    lab = models.ForeignKey(LabModel, on_delete=models.CASCADE, null=False, related_name="lab_instances")
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=False, related_name="lab_instances")
+
+    def __str__(self):
+        return f"{self.lab.name} - {self.user.pk} ({self.pk})"
